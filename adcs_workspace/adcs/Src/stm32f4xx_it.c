57a572561d475a9992f4ac26f53f2e04d20da9c9
nchronas <nchronas@gmail.com>
2016-05-11 07:30:12 -0700

    added ecss services support.

--- adcs_workspace/adcs/Src/stm32f4xx_it.c
+++ adcs_workspace/adcs/Src/stm32f4xx_it.c
@@ -106,7 +106,7 @@ void SPI1_IRQHandler(void)
 void USART2_IRQHandler(void)
 {
   /* USER CODE BEGIN USART2_IRQn 0 */
-
+  HAL_ADCS_UART_IRQHandler(&huart2);
   /* USER CODE END USART2_IRQn 0 */
   HAL_UART_IRQHandler(&huart2);
   /* USER CODE BEGIN USART2_IRQn 1 */
