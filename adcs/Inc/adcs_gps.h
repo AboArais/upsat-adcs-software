cc30a349dfcd62090468e05b2ea043abae43f72d
zisi <azisi@windowslive.com>
2016-08-07 17:32:05 +0300

    Update to set up RTC alarm and get GPS sentences.

--- adcs/Inc/adcs_gps.h
+++ adcs/Inc/adcs_gps.h
@@ -8,27 +8,20 @@
 #ifndef INC_ADCS_GPS_H_
 #define INC_ADCS_GPS_H_
 
-#include "services.h"
-#include "time_management_service.h"
+#include "stm32f4xx_hal.h"
+#include "adcs_time.h"
 #include "gps.h"
 
-#define GPS_ALARM_HOUR  5
-#define GPS_ALARM_MIN   20
+void init_gps_uart();
+void get_gps_sentences();
 
-#define GPS_ALARM_BASE_ADDRESS   0x01F000
-#define GPS_ALARM_OFFSET_ADDRESS 1
-
-extern uint8_t gps_sentence[NMEA_MAX_LEN];
-
-_gps_status gps_init(struct time_utc gps_utc);
+void HAL_GPS_Alarm_Handler(_gps_state *gps_state_value);
+_gps_status HAL_SetAlarm_GPS_ON(struct time_utc gps_utc);
+_gps_status HAL_SetAlarm_GPS_LOCK(struct time_utc gps_utc,
+        uint8_t gps_alarm_ulock_min);
+HAL_StatusTypeDef HAL_SetAlarm_GPS(uint8_t gps_alarm_hour,
+        uint8_t gps_alarm_min, uint8_t gps_alarm_sec);
 void HAL_GPS_UART_IRQHandler(UART_HandleTypeDef *huart);
 void UART_GPS_Receive_IT(UART_HandleTypeDef *huart);
-void reset_gps_flag();
-uint8_t get_gps_flag();
-_gps_status gps_status_flash(uint8_t gps_status_value);
-void HAL_GPS_Alarm_Handler(_gps_status gps_status_value);
-_gps_status HAL_SetAlarm_GPS_ON(struct time_utc gps_utc);
-_gps_status HAL_SetAlarm_GPS_LOCK(struct time_utc gps_utc);
-_gps_status HAL_SetAlarm_GPS(uint8_t gps_alarm_hour, uint8_t gps_alarm_min, uint8_t gps_alarm_sec);
 
 #endif /* INC_ADCS_GPS_H_ */
--- adcs/Src/adcs_gps.c
+++ adcs/Src/adcs_gps.c
@@ -5,83 +5,100 @@
  *      Author: azisi
  */
 
-#include "stm32f4xx_hal.h"
-
 #include "adcs_gps.h"
 #include "adcs_flash.h"
 #include "adcs_switch.h"
+#include "adcs_configuration.h"
 
-static uint8_t gps[NMEA_MAX_LEN];
+#include "sysview.h"
+
+static void reset_gps_flag();
+static uint8_t get_gps_flag();
+
+static uint8_t gps_buffer[NMEA_MAX_LEN];
 static uint8_t gps_flag = false;
 
-uint8_t gps_sentence[NMEA_MAX_LEN];
+static uint8_t gps_sentence[NMEA_MAX_LEN];
+static uint8_t gps_parser[NMEA_MAX_FIELDS][NMEA_MAX_FIELD_SIZE];
 
 extern UART_HandleTypeDef huart4;
 extern RTC_HandleTypeDef hrtc;
 
 /**
- * @brief Initialize GPS serial and alarm.
- * @param uart_gps_buf
- * @param gps_utc
+ * Initialize GPS serial.
  */
-_gps_status gps_init(struct time_utc gps_utc) {
-
+void init_gps_uart() {
     /* Set up UART4 for GPS */
-    uint8_t uart_gps_buf[NMEA_MAX_LEN];
-    memset(uart_gps_buf, 0, NMEA_MAX_LEN);
     memset(gps_sentence, 0, NMEA_MAX_LEN);
-    HAL_UART_Receive_IT(&huart4, &uart_gps_buf, NMEA_MAX_LEN);
+    HAL_UART_Receive_IT(&huart4, gps_buffer, NMEA_MAX_LEN);
+}
 
-    uint32_t flash_read_address = GPS_ALARM_BASE_ADDRESS;
-    uint8_t gps_flash[4];
-    flash_status gps_flash_status = FLASH_NORMAL;
-
-    memset(gps_flash, 0, 4);
-    for (uint8_t i = 0; i < 4; i++) {
-        if (flash_read_byte(&gps_flash[i], flash_read_address) == FLASH_NORMAL) {
-            flash_read_address = flash_read_address + GPS_ALARM_OFFSET_ADDRESS;
-        } else {
-            gps_flash_status = FLASH_ERROR;
-            break;
+/**
+ * Get 16 sentences and check if is correct. If it's correct get the arguments.
+ */
+void get_gps_sentences() {
+    /* Get 16 sentences for GPS to parse */
+    for (uint8_t i = 0; i < 2 * NMEA_NUM_SENTENCES; i++) {
+        if (get_gps_flag() == true) {
+            reset_gps_flag();
+            gps_parse_fields(&gps_sentence, NMEA_MAX_LEN, gps_parser);
+            gps_parse_logic(&gps_parser, &gps_state);
         }
     }
-    /* Is not the first boot and the time is set up from OBC and no error in flash */
-    if (adcs_boot_cnt > 1 && gps_utc.year != 0 && gps_flash_status == FLASH_NORMAL) {
-        if (HAL_SetAlarm_GPS(gps_flash[0], gps_flash[1], gps_flash[2]) == GPS_NORMAL) {
-            return gps_status_flash(gps_flash[3]);
-        } else {
-            return GPS_ERROR;
-        }
-    /* First time that ADCS boot or the time is not set up from OBC or error with flash */
-    } else {
-        if (gps_flash_status == FLASH_ERROR) {
-            return GPS_ERROR;
-        } else {
-            return HAL_SetAlarm_GPS_ON(gps_utc);
-        }
+}
+
+/**
+ * RTC alarm handler and set the next RTC alarm
+ * @param gps_state_value: update the gps_state status
+ */
+void HAL_GPS_Alarm_Handler(_gps_state *gps_state_value) {
+
+    struct time_utc gps_utc_handler;
+    get_time_UTC(&gps_utc_handler);
+
+    switch (gps_state_value->status) {
+    case GPS_OFF:
+        adcs_pwr_switch(SWITCH_ON, GPS);
+        gps_state_value->status = HAL_SetAlarm_GPS_LOCK(gps_utc_handler,
+        GPS_ALARM_UNLOCK);
+        SYSVIEW_PRINT("GPS UNLOCK");
+        break;
+    case GPS_UNLOCK:
+        gps_state_value->reset_flag = true;
+        gps_state_value->status = HAL_SetAlarm_GPS_LOCK(gps_utc_handler,
+        GPS_ALARM_RESET);
+        SYSVIEW_PRINT("GPS RESET");
+        break;
+    case GPS_RESET:
+        adcs_pwr_switch(SWITCH_OFF, GPS);
+        gps_state_value->status = HAL_SetAlarm_GPS_ON(gps_utc_handler);
+        SYSVIEW_PRINT("GPS OFF");
+        break;
+    default:
+        break;
     }
 
 }
 
 /**
- * @brief  This function handles UART interrupt request.
+ * This function handles UART interrupt request.
  * @param  huart: pointer to a UART_HandleTypeDef structure that contains
  *                the configuration information for the specified UART module.
  * @retval None
  */
 void HAL_GPS_UART_IRQHandler(UART_HandleTypeDef *huart) {
-    uint32_t tmp1 = 0U, tmp2 = 0U;
+    uint32_t gps_uart_tmp1, gps_uart_tmp2;
 
-    tmp1 = __HAL_UART_GET_FLAG(huart, UART_FLAG_RXNE);
-    tmp2 = __HAL_UART_GET_IT_SOURCE(huart, UART_IT_RXNE);
+    gps_uart_tmp1 = __HAL_UART_GET_FLAG(huart, UART_FLAG_RXNE);
+    gps_uart_tmp2 = __HAL_UART_GET_IT_SOURCE(huart, UART_IT_RXNE);
     /* UART in mode Receiver ---------------------------------------------------*/
-    if ((tmp1 != RESET) && (tmp2 != RESET)) {
+    if ((gps_uart_tmp1 != RESET) && (gps_uart_tmp2 != RESET)) {
         UART_GPS_Receive_IT(huart);
     }
 }
 
 /**
- * @brief  Receives an amount of data in non blocking mode
+ * Receives an amount of data in non blocking mode
  * @param  huart: pointer to a UART_HandleTypeDef structure that contains
  *                the configuration information for the specified UART module.
  * @retval HAL status
@@ -108,10 +125,10 @@ void UART_GPS_Receive_IT(UART_HandleTypeDef *huart) {
         huart->RxXferCount--;
         *huart->pRxBuffPtr++ = 0;
         gps_flag = true;
-        huart->pRxBuffPtr = &gps;
-        memset(gps_sentence, 0 , NMEA_MAX_LEN);
-        for (uint8_t i = 0 ; i < NMEA_MAX_LEN; i++){
-            gps_sentence[i] = gps[i];
+        huart->pRxBuffPtr = &gps_buffer;
+        memset(gps_sentence, 0, NMEA_MAX_LEN);
+        for (uint8_t i = 0; i < NMEA_MAX_LEN; i++) {
+            gps_sentence[i] = gps_buffer[i];
         }
         huart->RxXferCount = huart->RxXferSize;
     } else if (huart->RxXferSize > huart->RxXferCount) {
@@ -126,55 +143,41 @@ void UART_GPS_Receive_IT(UART_HandleTypeDef *huart) {
 
 }
 
+/**
+ * Reset GPS flag when the parser get a correct sentence
+ */
 void reset_gps_flag() {
-
     gps_flag = false;
 }
 
+/**
+ * Get GPS flag to check if the parser has a correct sentence
+ */
 uint8_t get_gps_flag() {
-
     return gps_flag;
 }
 
-void HAL_GPS_Alarm_Handler(_gps_status gps_status_value) {
-    struct time_utc gps_utc_handler;
-    switch (gps_status_value) {
-    case GPS_OFF:
-        adcs_pwr_switch(SWITCH_ON, GPS);
-        get_time_UTC(&gps_utc_handler);
-        gps_state.status = HAL_SetAlarm_GPS_LOCK(gps_utc_handler);
-        break;
-    case GPS_UNLOCK:
-        gps_state.status = GPS_ERROR;
-        break;
-    case GPS_LOCK:
-        gps_state.status = GPS_OFF;
-        break;
-    default:
-        break;
-    }
-}
-
 /**
- * @brief Set up GPS alarm, to open GPS every 5 hours and save in flash the
- *        alarm time and GPS status.
- * @param gps_utc
+ * Set up GPS alarm, to open GPS every 5 hours and save in flash the alarm time
+ * and GPS status.
+ * @param gps_utc: Get UTC time to set RTC alarm
+ * @return _gps_status
  */
 _gps_status HAL_SetAlarm_GPS_ON(struct time_utc gps_utc) {
 
-    uint8_t gps_flash[3];
-    memset(gps_flash, 0, 3);
+    uint8_t gps_flash[4];
+    memset(gps_flash, 0, 4);
 
-    gps_flash[0] = (gps_utc.hour + GPS_ALARM_HOUR) % 24;
+    gps_flash[0] = (gps_utc.hour + GPS_ALARM_ON) % 24;
     gps_flash[1] = gps_utc.min;
     gps_flash[2] = gps_utc.sec;
+    gps_flash[3] = GPS_OFF;
     HAL_SetAlarm_GPS(gps_flash[0], gps_flash[1], gps_flash[2]);
-
     /* Write to flash GPS alarm to recover after reset */
     if (flash_erase_block4K(GPS_ALARM_BASE_ADDRESS) == FLASH_NORMAL) {
-        flash_write_page(gps_flash, GPS_ALARM_BASE_ADDRESS);
+        flash_write_page(gps_flash, sizeof(gps_flash), GPS_ALARM_BASE_ADDRESS);
     } else {
-        return GPS_ERROR;
+        return GPS_ERROR_FLASH;
     }
 
     return GPS_OFF;
@@ -182,43 +185,57 @@ _gps_status HAL_SetAlarm_GPS_ON(struct time_utc gps_utc) {
 }
 
 /**
- * @brief Set up GPS alarm, to wait 20 minutes for cold start and save in flash
- *        the alarm and GPS status.
- * @param gps_utc
+ * Set up GPS alarm, to wait in total 18 minutes for cold start - reset -
+ * cold start and save in flash the alarm and GPS status.
+ * @param gps_utc: Get UTC time to set RTC alarm
+ * @return _gps_status
  */
-_gps_status HAL_SetAlarm_GPS_LOCK(struct time_utc gps_utc) {
+_gps_status HAL_SetAlarm_GPS_LOCK(struct time_utc gps_utc,
+        uint8_t gps_alarm_ulock_min) {
 
-    uint8_t gps_flash[3];
+    uint8_t gps_flash[4];
 
-    memset(gps_flash, 0, 3);
+    memset(gps_flash, 0, 4);
 
-    if (gps_utc.min + GPS_ALARM_MIN > 60) {
+    if (gps_utc.min + gps_alarm_ulock_min > 60) {
         gps_flash[0] = gps_utc.hour + 1;
     } else {
         gps_flash[0] = gps_utc.hour;
     }
 
-    gps_flash[1] = (gps_utc.min + GPS_ALARM_MIN) % 60;
+    gps_flash[1] = (gps_utc.min + gps_alarm_ulock_min) % 60;
     gps_flash[2] = gps_utc.sec;
     HAL_SetAlarm_GPS(gps_flash[0], gps_flash[1], gps_flash[2]);
-
+    /* Write to flash the correct GPS state */
+    if (gps_alarm_ulock_min == GPS_ALARM_UNLOCK) {
+        gps_flash[3] = GPS_UNLOCK;
+    } else {
+        gps_flash[3] = GPS_RESET;
+    }
     /* Write to flash GPS alarm to recover after reset */
     if (flash_erase_block4K(GPS_ALARM_BASE_ADDRESS) == FLASH_NORMAL) {
-        flash_write_page(gps_flash, GPS_ALARM_BASE_ADDRESS);
+        flash_write_page(gps_flash, sizeof(gps_flash), GPS_ALARM_BASE_ADDRESS);
     } else {
-        return GPS_ERROR;
+        return GPS_ERROR_FLASH;
     }
 
-    return GPS_ON;
+    if (gps_alarm_ulock_min == GPS_ALARM_UNLOCK) {
+        return GPS_UNLOCK;
+    } else {
+        return GPS_RESET;
+    }
+
 }
 
 /**
- * @brief Set GPS alarm to interrupt every day at specific time.
+ * Set GPS alarm to interrupt every day at specific time.
  * @param gps_alarm_hour
  * @param gps_alarm_min
  * @param gps_alarm_sec
+ * @return HAL_StatusTypeDef: return type to evaluate if HAL works correctly
  */
-_gps_status HAL_SetAlarm_GPS(uint8_t gps_alarm_hour, uint8_t gps_alarm_min, uint8_t gps_alarm_sec) {
+HAL_StatusTypeDef HAL_SetAlarm_GPS(uint8_t gps_alarm_hour,
+        uint8_t gps_alarm_min, uint8_t gps_alarm_sec) {
 
     RTC_AlarmTypeDef gps_alarm;
 
@@ -233,29 +250,8 @@ _gps_status HAL_SetAlarm_GPS(uint8_t gps_alarm_hour, uint8_t gps_alarm_min, uint
     gps_alarm.AlarmDateWeekDaySel = RTC_ALARMDATEWEEKDAYSEL_DATE;
     gps_alarm.AlarmDateWeekDay = 1;
     gps_alarm.Alarm = RTC_ALARM_A;
-    if (HAL_RTC_SetAlarm_IT(&hrtc, &gps_alarm, RTC_FORMAT_BIN) != HAL_OK) {
-        return GPS_ERROR;
-    }
+    HAL_RTC_SetAlarm_IT(&hrtc, &gps_alarm, RTC_FORMAT_BIN);
 
-    return GPS_NORMAL;
+    return HAL_OK;
 
-}
-
-_gps_status gps_status_flash(uint8_t gps_status_value) {
-    switch (gps_status_value) {
-    case (0):
-        return GPS_ERROR;
-    case (1):
-        return GPS_NORMAL;
-    case (2):
-        return GPS_ON;
-    case (3):
-        return GPS_OFF;
-    case (4):
-        return GPS_LOCK;
-    case (5):
-        return GPS_UNLOCK;
-    default:
-        return GPS_ERROR;
-    }
 }
